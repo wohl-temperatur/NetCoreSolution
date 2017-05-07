0d7995af13cfca5be78464a1dbf8147168972b56
Kensan(PC-Z560) <Kensan@PC-Z560>
2017-05-08 00:09:52 +0900

    first commit

---
+++ NetCoreApplication/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NetCoreApplication
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            // 以下のファイル名に一致するファイルを静的コンテンツのリストから探して一番最初に見つかったものを実行時に表示させます。
+            // 　・default.htm
+            // 　・default.html
+            // 　・index.htm
+            // 　・index.html
+            app.UseDefaultFiles();
+
+            // web root (既定 は wwwroot) のファイルを静的コンテンツとして扱うようにします。
+            app.UseStaticFiles();
+        }
+    }
+}
